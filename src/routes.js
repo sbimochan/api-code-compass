eca08191f5fec82e59f2a601dfcbeca6b5071244
b7Rexx <29946706+b7Rexx@users.noreply.github.com>
2024-10-08 22:46:37 +0545

    auth login api with response token

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import swaggerSpec from './utils/swagger';
 import userRoutes from './routes/userRoutes';
 import roleRoutes from './routes/roleRoutes';
+import authRoutes from './routes/authRoutes';
 import movieRoutes from './routes/movieRoutes';
 import rentalRoutes from './routes/rentalRoutes';
 
@@ -32,5 +33,6 @@ router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
 router.use('/rents', rentalRoutes);
 router.use('/roles', roleRoutes);
+router.use('/', authRoutes);
 
 export default router;
