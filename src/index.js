c81b1a9a4dd856a11640e9fa0e73504c40a45c9d
abhigyandhakallf <abhigyandhakal@lftechnology.com>
2024-10-29 16:39:42 +0545

    fix/Fix swagger API doc initial url

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './instrument';
 import './env';
 import './db';
 
-import fs from 'fs';
 import cors from 'cors';
 import path from 'path';
 import helmet from 'helmet';
@@ -12,12 +11,14 @@ import favicon from 'serve-favicon';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import * as Sentry from '@sentry/node';
+import swaggerUi from 'swagger-ui-express';
 
 import routes from './routes';
 
 import json from '@middlewares/json';
 import * as errorHandler from '@middlewares/errorHandler';
 
+import swaggerSpec from '@utils/swagger';
 import logger, { logStream } from '@utils/logger';
 
 const app = express();
@@ -25,8 +26,6 @@ const app = express();
 const APP_PORT =
   (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || process.env.PORT || '3000';
 const APP_HOST = process.env.APP_HOST || '0.0.0.0';
-
-const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
 app.set('port', APP_PORT);
 app.set('host', APP_HOST);
@@ -46,17 +45,8 @@ app.use(json);
 // API Routes
 app.use('/api', routes);
 
-// Swagger UI
-// Workaround for changing the default URL in swagger.json
-// https://github.com/swagger-api/swagger-ui/issues/4624
-const swaggerIndexContent = fs
-  .readFileSync(`${pathToSwaggerUi}/index.html`)
-  .toString()
-  .replace('https://petstore.swagger.io/v2/swagger.json', '/api/swagger.json');
-
-app.get('/api-docs/index.html', (req, res) => res.send(swaggerIndexContent));
 app.get('/api-docs', (req, res) => res.redirect('/api-docs/index.html'));
-app.use('/api-docs', express.static(pathToSwaggerUi));
+app.use('/api-docs/index.html', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Error Middleware
 app.use(errorHandler.genericErrorHandler);
